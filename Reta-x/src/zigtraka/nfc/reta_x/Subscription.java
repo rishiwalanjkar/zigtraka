5a55dcbd9cbbfe1b4b2a9b42ad730720b36cf6dc
rishiwalanjkar <rishi.walanjkar@gmail.com>
2013-11-12 18:45:49 +0530

    b2 replaced into master 13/11/13 6:45 pm

--- Reta-x/src/zigtraka/nfc/reta_x/Subscription.java
+++ Reta-x/src/zigtraka/nfc/reta_x/Subscription.java
@@ -1,13 +1,31 @@
 package zigtraka.nfc.reta_x;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class Subscription extends BaseActivity {
+
+	private TextView Activate,DaysRemain,Green;
+	private EditText EnterKey,Name,Email;
+	private Spinner Spinner1;
+	private Button Renew;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Activate=(TextView)findViewById(R.id.Subscription_ActivetextView);
+		DaysRemain=(TextView)findViewById(R.id.Subscription_remaintextView);
+		EnterKey=(EditText)findViewById(R.id.Subscription_enterkeyedit);
+		Spinner1=(Spinner) findViewById(R.id.Subscription_spinner1);
+		Renew=(Button)findViewById(R.id.Subscription_button);
+		Name=(EditText)findViewById(R.id.Subscription_nameedit);
+		Email=(EditText)findViewById(R.id.Subscription_emailedit);
+		Green=(TextView)findViewById(R.id.Subscription_Greentext);
+		
 		
 	}
 
